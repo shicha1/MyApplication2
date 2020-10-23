c7f72ba0655072ea1742a02f89b6feeac9b7d4ed
杨鹏 <707042815@qq.com>
2020-10-23 19:41:52 +0800

    listView 两种显示方法，微调布局

--- app/src/main/java/com/example/hp/myapplication1/fragment/ImageItem.java
+++ app/src/main/java/com/example/hp/myapplication1/fragment/ImageItem.java
@@ -3,7 +3,6 @@ package com.example.hp.myapplication1.fragment;
 import com.example.hp.myapplication1.R;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class ImageItem {
